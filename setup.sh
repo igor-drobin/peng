9a3d4485a0a3e1e75f3ec90b7cc3dae7bd8af8be
igor-drobin <66243036+igor-drobin@users.noreply.github.com>
2020-07-20 15:39:52 +0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
-mkdir -p ~/.streamlit
+mkdir -p ~/.streamlit/
+
+echo "[general]
+email = \"igor.drobin@gmail.com\"
+" > ~/.streamlit/credentials.toml
 
 echo "[server]
 headless = true
+enableCORS=false
 port = $PORT
-enableCORS = false
 " > ~/.streamlit/config.toml
