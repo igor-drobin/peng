23fc6c2140d4a28a7e0a56f5c6beab6397a1e641
igor-drobin <66243036+igor-drobin@users.noreply.github.com>
2020-07-20 12:56:37 +0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+st.write("""
+# Penguin Prediction App
+This app predicts the **Palmer Penguin** species!
+Data obtained from the [palmerpenguins library](https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst.
+""")
