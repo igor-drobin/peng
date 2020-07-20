c5154f9f9d98f3c86297bb872aa76a8a18138e00
igor-drobin <66243036+igor-drobin@users.noreply.github.com>
2020-07-20 15:00:18 +0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,9 +5,8 @@ echo "\
 email = \"igor.drobin@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
